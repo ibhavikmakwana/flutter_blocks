ea4739f2d1f5612b99e8baad73b5e7baa3486a77
ibhavikmakwana <bhavikmakwana43@gmail.com>
2020-07-09 18:49:20 +0530

    :recycle: Refactoring

--- lib/modules/home/main_screen.dart
+++ lib/modules/home/main_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:flutterblocks/modules/blog/blog_one.dart';
+import 'package:flutterblocks/modules/blog/bog_one/blog_one.dart';
 import 'package:flutterblocks/modules/drawer/drawer_widget.dart';
 import 'package:flutterblocks/store/theme/theme_store.dart';
 import 'package:flutterblocks/values/themes.dart';
